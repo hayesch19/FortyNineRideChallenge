0cfcb57315a947efdd0ad9b25d4954d0677ee04e
hayesch19 <chrishayespro@gmail.com>
2019-09-30 11:47:44 -0400

    Added DWParksController End Points

---
+++ Controllers/DWParksController.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using System.Linq;
+using FortyNineRideChallenge.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FortyNineRideChallenge.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DWParksController : ControllerBase
+  {
+    private DatabaseContext context;
+    public DWParksController(DatabaseContext _context)
+    {
+      this.context = _context;
+    }
+
+    // Add A Park
+    [HttpPost]
+    public ActionResult<DisneyWorldParks> CreateRide([FromBody] DisneyWorldParks entry)
+    {
+      context.DisneyWorldPark.Add(entry);
+      context.SaveChanges();
+      return entry;
+    }
+
+    // Get All Parks
+    [HttpGet]
+    public ActionResult<IEnumerable<DisneyWorldParks>> GetAllParks()
+    {
+      var parks = context.DisneyWorldPark.OrderByDescending(park => park.Id);
+      return parks.ToList();
+    }
+
+    // Get A Park
+    [HttpGet("{id}")]
+    public ActionResult GetOnePark(int id)
+    {
+      var park = context.DisneyWorldPark.FirstOrDefault(p => p.Id == id);
+      if (park == null)
+      {
+        return NotFound();
+      }
+      else
+      {
+        return Ok(park);
+      }
+    }
+
+    // Update Park
+    [HttpPut("{id}")]
+    public ActionResult<DisneyWorldParks> UpdateRide(int id, [FromBody]DisneyWorldParks newDetails)
+    {
+      if (id != newDetails.Id)
+      {
+        return BadRequest();
+      }
+      context.Entry(newDetails).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+      context.SaveChanges();
+      return newDetails;
+    }
+
+    // Delete Park
+    [HttpDelete("{id}")]
+    public ActionResult<DisneyWorldParks> DeletePark([FromBody]DisneyWorldParks entry, int id)
+    {
+      var parkToDelete = context.DisneyWorldPark.FirstOrDefault(park => park.Id == id);
+      context.DisneyWorldPark.Remove(parkToDelete);
+      context.SaveChanges();
+      return parkToDelete;
+    }
+  }
+}
