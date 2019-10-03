c69a7b3be132b79b4266f4e2d88939b596622fea
hayesch19 <chrishayespro@gmail.com>
2019-10-03 14:18:57 -0400

    Menu Updated

--- ClientApp/src/components/HeaderNav.jsx
+++ ClientApp/src/components/HeaderNav.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const HeaderNav = () => {
+  const element = <FontAwesomeIcon icon={faBars} />
+
   return (
     <section className="top-container">
       <div className="top-left">
@@ -11,11 +15,17 @@ const HeaderNav = () => {
       </div>
       <div className="top-right">
         <div className="dropdown">
-          <button className="dropbtn">Menu</button>
+          <button className="dropbtn">{element}</button>
           <div className="dropdown-content">
-            <Link to="/">Home</Link>
-            <Link to="/scores">Scores</Link>
-            <Link to="/rules">Rules</Link>
+            <Link className="dropdown-link" to="/">
+              Home
+            </Link>
+            <Link className="dropdown-link" to="/scores">
+              Scores
+            </Link>
+            <Link className="dropdown-link" to="/rules">
+              Rules
+            </Link>
           </div>
         </div>
       </div>
--- ClientApp/src/components/RideNav.jsx
+++ ClientApp/src/components/RideNav.jsx
@@ -3,11 +3,19 @@ import { Link } from 'react-router-dom'
 
 const RideNav = () => {
   return (
-    <div>
-      <Link to="/magickingdom">Magic Kingdom</Link>
-      <Link to="/animalkingdom">Animal Kingdom</Link>
-      <Link to="/hollywoodstudios">Hollywood Studios</Link>
-      <Link to="/epcot">Epcot</Link>
+    <div className="ride-nav">
+      <Link className="ride-nav" to="/magickingdom">
+        Magic Kingdom
+      </Link>
+      <Link className="ride-nav" to="/animalkingdom">
+        Animal Kingdom
+      </Link>
+      <Link className="ride-nav" to="/hollywoodstudios">
+        Hollywood Studios
+      </Link>
+      <Link className="ride-nav" to="/epcot">
+        Epcot
+      </Link>
     </div>
   )
 }
