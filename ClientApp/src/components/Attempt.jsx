6bc63263079789e6513c5fb67e2a1ed74465456e
hayesch19 <chrishayespro@gmail.com>
2019-10-10 13:36:55 -0400

    Scores Page Display Updated

--- ClientApp/src/components/Attempt.jsx
+++ ClientApp/src/components/Attempt.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
-import moment from 'moment'
+import moment, { invalid } from 'moment'
 
 const Attempt = props => {
+  // Find Total Time
   const x = new moment(props.timeStarted)
   const y = new moment(props.timeEnded)
   const duration = moment.duration(y.diff(x))
 
+  const isOnGoing = props.timeEnded === null
+  console.log(isOnGoing)
+
   return (
     <>
       <h3>{moment(props.timeStarted).format('MMMM Do YYYY,')}</h3>
-      <p>Time Started: {moment(props.timeStarted).format('hh:mm a')}</p>
-      <p>Time Ended: {moment(props.timeEnded).format('hh:mm a')}</p>
-      <p>
-        Total Time: {duration.get('hours')} hours &amp;&nbsp;
-        {duration.get('minutes')} minutes
-      </p>
       <p>Rides Completed: {props.ridesCompleted}</p>
+      <p>Time Started: {moment(props.timeStarted).format('hh:mm a')}</p>
+      <p>
+        Time Ended:{' '}
+        {isOnGoing ? 'On Going' : moment(props.timeEnded).format('hh:mm a')}
+      </p>
+      <p>
+        {isOnGoing ? '' : 'Total Time:'}&nbsp;
+        {isOnGoing ? '' : duration.get('hours')}&nbsp;
+        {isOnGoing ? '' : 'hours'}&nbsp;
+        {isOnGoing ? '' : '&'}&nbsp;
+        {isOnGoing ? '' : duration.get('minutes')}&nbsp;
+        {isOnGoing ? '' : 'minutes'}
+      </p>
     </>
   )
 }
@@ -24,4 +35,6 @@ export default Attempt
 
 {
   /* <p>humanize: {duration.locale('en').humanize()}</p> */
+  // Total Time: {isOnGoing ? 'On Going' : duration.get('hours')} hours
+  // &amp;&nbsp; {isOnGoing ? '' : duration.get('minutes')} minutes
 }
